5cb45270bc41863efa000929092be7e2b89a8422
alexandre <alexandre.ward@ecole-hexagone.com>
2022-03-31 21:52:20 +0200

    made changes

--- src/Components/ArticleModal.jsx
+++ src/Components/ArticleModal.jsx
@@ -1,43 +1,60 @@
 import React, { Component } from 'react';
 import { Modal, Button } from 'antd';
 import ArticleForm from './ArticleForm';
+import Fire from '../Fire';
 
 export default class ArticleModal extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      title: "",
-      content: "",
-    }
-  }
+	constructor() {
+		super();
+		this.state = {
+			title: "",
+			content: "",
+		}
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const firebase = new Fire(error => {
+			if (error) {
+				console.error(error);
+			} else {
+				firebase.addArticle({
+					title: this.state.title,
+					content: this.state.content,
+					createdAt: new Date(),
+					comments: []
+				})
+			}
+		})
+		this.props.handleCancel();
+	}
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    alert("titre : " + this.state.title + "\n contenu : " + this.state.content);
-    this.props.handleCancel();
-    this.setState({ title: "", content: ""})
-  }
+	handleChange = (e) => {
+		this.setState(e.target.id === "title" ? { title: e.target.value } : { content: e.target.value });
 
-  handleChange = (e) => {
-    this.setState(e.target.id === "title" ? { title: e.target.value } : { content: e.target.value });
+	}
 
-  }
+	componentWillUnmount() {
+		this.setState({
+			title: "",
+			content: "",
+		})
+	}
 
-  render() {
-    return <Modal
-      title="Créer un article"
-      visible={this.props.isVisible}
-      footer={[
-        <Button key="submit" onClick={this.handleSubmit}>Valider</Button>
-      ]}
-      onCancel={this.props.handleCancel}
-    >
-      <ArticleForm
-        title={this.props.title}
-        handleChange={this.handleChange}
-        handleSubmit={this.handleSubmit}
-      />
-    </Modal>;
-  }
+	render() {
+		return <Modal
+			title="Créer un article"
+			visible={this.props.isVisible}
+			footer={[
+				<Button key="submit" onClick={this.handleSubmit}>Valider</Button>
+			]}
+			onCancel={this.props.handleCancel}
+		>
+			<ArticleForm
+				title={this.props.title}
+				handleChange={this.handleChange}
+				handleSubmit={this.handleSubmit}
+			/>
+		</Modal>;
+	}
 }
--- src/Fire.js
+++ src/Fire.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCC6BDMwt--ro-qvFFQphToTDXC-jCIW50",
-    authDomain: "video-jeu.firebaseapp.com",
-    projectId: "video-jeu",
-    storageBucket: "video-jeu.appspot.com",
-    messagingSenderId: "810896795873",
-    appId: "1:810896795873:web:2b88d97e49291b95fb6fa0"
-  };
+    apiKey: "AIzaSyCGOG1Er054mWiDHpvdu1MPrCPGdlfCdmQ",
+    authDomain: "react-article-da8d6.firebaseapp.com",
+    projectId: "react-article-da8d6",
+    storageBucket: "react-article-da8d6.appspot.com",
+    messagingSenderId: "938706405579",
+    appId: "1:938706405579:web:7c97f007252f947165b34e"
+};
 
 export default class Fire {
     constructor(callback) {
@@ -30,7 +30,7 @@ export default class Fire {
     }
 
     getArticles(callback) {
-        let ref = this.ref.orderBy("date_creation");
+        let ref = this.ref.orderBy("createdAt");
         this.unsubscribe = ref.onSnapshot(snapshot => {
             let articles = [];
             snapshot.forEach(doc => {
